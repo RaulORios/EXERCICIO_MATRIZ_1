f88779cf2b515a9e26b007e09dc4ac59bb575685
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:55:22 -0300

    Program.cs adicionado

---
+++ src/EXERCICIO_MATRIZ_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EXERCICIO_MATRIZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[,] a;
+
+            n = int.Parse(Console.ReadLine());
+            a = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonais principais: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i, i]);
+            }
+            Console.WriteLine();
+
+            int totnegativo = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i,j] < 0)
+                    {
+                        totnegativo++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Quantidade de negativos: {totnegativo}");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
